f5b5748bda7defd6aed37a312f28f719f02e99e2
maximezim <maxime.zimmermann57@gmail.com>
2024-09-12 00:19:00 +0200

    containerization

--- authentication/src/main/java/com/learnandphish/authentication/AuthenticationApplication.java
+++ authentication/src/main/java/com/learnandphish/authentication/AuthenticationApplication.java
@@ -2,12 +2,14 @@ package com.learnandphish.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  *
  * @author Robin Lafontaine
  */
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.learnandphish.authentication")
 public class AuthenticationApplication {
 
 	public static void main(String[] args) {
--- authentication/src/main/java/com/learnandphish/authentication/JwtAuthenticationEntryPoint.java
+++ authentication/src/main/java/com/learnandphish/authentication/JwtAuthenticationEntryPoint.java
@@ -5,9 +5,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
