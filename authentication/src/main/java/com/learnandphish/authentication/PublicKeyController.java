f76f6893e4e2517d06c7abdd0e3e75b56ef5b62d
maximezim <maxime.zimmermann57@gmail.com>
2024-09-15 15:53:19 +0200

    #14 keep previous key when authentication service restarts

--- authentication/src/main/java/com/learnandphish/authentication/PublicKeyController.java
+++ authentication/src/main/java/com/learnandphish/authentication/PublicKeyController.java
@@ -3,8 +3,6 @@ package com.learnandphish.authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.security.PublicKey;
 import java.util.Base64;
@@ -18,7 +16,6 @@ public class PublicKeyController {
     @GetMapping("/public-key")
     public String getPublicKey() {
         PublicKey publicKey = jwtUtil.getPublicKey();
-        String encodedKey = Base64.getEncoder().encodeToString(publicKey.getEncoded());
-        return encodedKey;
+        return Base64.getEncoder().encodeToString(publicKey.getEncoded());
     }
 }
