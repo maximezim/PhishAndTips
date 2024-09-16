8221f6364f658448b4f40d8489029fbf4bdec15e
maximezim <maxime.zimmermann57@gmail.com>
2024-09-16 18:00:30 +0200

    removed useless stuff

--- gateway/src/main/java/com/learnandphish/gateway/GatewayConfig.java
+++ gateway/src/main/java/com/learnandphish/gateway/GatewayConfig.java
@@ -25,32 +25,6 @@ public class GatewayConfig {
         this.authFilter = authFilter;
     }
 
-//    @Bean
-//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-//        return builder.routes()
-//                .route("first-microservice", r -> r.path("/first")
-//                        .and().method("POST")
-//                        .and().readBody(Student.class, s -> true)
-//                        .filters(f -> f.filters(requestFilter, authFilter))
-//                        .uri("http://localhost:8081"))
-//                .route("first-microservice", r -> r.path("/first")
-//                        .and().method("GET")
-//                        .filters(f -> f.filters(authFilter))
-//                        .uri("http://localhost:8081"))
-//                .route("second-microservice", r -> r.path("/second")
-//                        .and().method("POST")
-//                        .and().readBody(Company.class, s -> true)
-//                        .filters(f -> f.filters(requestFilter, authFilter))
-//                        .uri("http://localhost:8082"))
-//                .route("second-microservice", r -> r.path("/second")
-//                        .and().method("GET")
-//                        .filters(f -> f.filters(authFilter))
-//                        .uri("http://localhost:8082"))
-//                .route("auth-server", r -> r.path("/login")
-//                        .uri("http://localhost:8088"))
-//                .build();
-//    }
-
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
