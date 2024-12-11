28c4434b9e932d9fe3c49f14d332a1c1edb31c6c
robinlafontaine <robin@lafontaines.eu>
2024-12-11 16:28:19 +0100

    refactor: add getters/setters in UserDTO

--- authentication/src/main/java/com/learnandphish/authentication/UserDTO.java
+++ authentication/src/main/java/com/learnandphish/authentication/UserDTO.java
@@ -19,24 +19,4 @@ public class UserDTO {
             this.role = role;
         }
 
-        public void setFirstName(String firstName) {
-            this.firstName = firstName;
-        }
-
-        public void setLastName(String lastName) {
-            this.lastName = lastName;
-        }
-
-        public void setEmail(String email) {
-            this.email = email;
-        }
-
-        public void setPosition(String position) {
-            this.position = position;
-        }
-
-        public void setRole(Roles role) {
-            this.role = role;
-        }
-
 }
