911e2eef35d5dc1c37469c4f6a1575d32e81f08a
Bastub <bastub57@gmail.com>
2025-02-04 17:02:34 +0100

    feat: get user info

--- frontend/src/lib/components/custom/header.svelte
+++ frontend/src/lib/components/custom/header.svelte
@@ -4,7 +4,10 @@
   import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
 	import { goto } from "$app/navigation";
   export let title: string;
-  export let nom: string;
+  export let firstName: string;
+  export let lastName: string;
+  export let position: string;
+  export let email: string;
   import AuthService from "$lib/services/AuthService";
 
   function handleLogout() {
@@ -20,13 +23,15 @@
         <DropdownMenu.Trigger asChild let:builder>
           <Button variant="outline" class="pe-5" builders={[builder]}>
             <iconify-icon class="me-2 text-base" icon="mingcute:user-3-fill"></iconify-icon>
-            {nom}
+            {firstName} {lastName}
         </Button>
         </DropdownMenu.Trigger>
         <DropdownMenu.Content class="w-38">
           <DropdownMenu.CheckboxItem class="cursor-pointer" on:click={handleLogout}>
             Se déconnecter
           </DropdownMenu.CheckboxItem>
+          <p>{position}</p>
+          <p>{email}</p>
         </DropdownMenu.Content>
       </DropdownMenu.Root>
 </header>
--- frontend/src/routes/(app)/+layout.svelte
+++ frontend/src/routes/(app)/+layout.svelte
@@ -7,7 +7,18 @@
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
 
+	var firstName: "";
+	var lastName: "";
+	var position: "";
+	var email: "";
+
 	onMount(async() => {
+		const user = await fetch("/api/auth/get-user").then(res => res.json()); 
+		firstName = user.firstName;
+		lastName = user.lastName;
+		position = user.position;
+		email = user.email;
+
 		if (window.location.pathname === '/') {
 			goto('/dashboard');
 		}
@@ -31,7 +42,7 @@
 		phishing_bg= {$page.data?.phishing_bg}
 	/>
 	<main class="relative w-full h-full overflow-scroll flex flex-col">
-		<Header title={$page.data?.title || 'Phish&Tips'} nom={'Robin'}/>
+		<Header title={$page.data?.title || 'Phish&Tips'} firstName={firstName} lastName={lastName} position={position} email={email} />
 		<div class="relative flex-grow w-full overflow-scroll">
 			<DotPage />
 			<slot></slot>
