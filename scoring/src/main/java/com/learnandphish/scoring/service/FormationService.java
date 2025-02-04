7122a36d575c2254944ff97eca2311e607a5d1d3
robinlafontaine <robin@lafontaines.eu>
2025-02-04 16:28:54 +0100

    refactor: remove unused imports

--- scoring/src/main/java/com/learnandphish/scoring/service/FormationService.java
+++ scoring/src/main/java/com/learnandphish/scoring/service/FormationService.java
@@ -1,13 +1,11 @@
 package com.learnandphish.scoring.service;
 
 import com.learnandphish.scoring.entity.FormationDTO;
-import com.learnandphish.scoring.factory.AuthenticationDatabaseFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -26,7 +24,8 @@ public class FormationService {
                 url,
                 HttpMethod.GET,
                 null,
-                new ParameterizedTypeReference<List<FormationDTO>>() {}
+                new ParameterizedTypeReference<>() {
+                }
         );
 
         if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null) {
