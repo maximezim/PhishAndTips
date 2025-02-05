fb369738f8dcf40ccfad3095a3f84c22414ff310
Bastub <bastub57@gmail.com>
2025-02-05 10:27:39 +0100

    feat: add go back arrow

--- frontend/src/lib/components/custom/header.svelte
+++ frontend/src/lib/components/custom/header.svelte
@@ -8,7 +8,14 @@
   export let lastName: string;
   export let position: string;
   export let email: string;
+  export let isSubPage: boolean;
   import AuthService from "$lib/services/AuthService";
+  import { browser } from "$app/environment";
+
+  function nav_back() {
+      if (browser) window.history.back();
+  }
+  
 
   function handleLogout() {
     AuthService.deleteToken();
@@ -18,7 +25,14 @@
 </script>
 
 <header class={"w-full py-3 px-4 sticky top-0 bg-primary flex items-center justify-between z-10"}>
+  <div class={"flex flex-row align-middle gap-2"}>
+    {#if isSubPage}
+      <button on:click={nav_back} aria-label="Go back">
+        <iconify-icon class="flex flex-row align-middle icon-custom text-2xl text-primary-foreground" icon="mingcute:arrow-left-fill"></iconify-icon>
+      </button>
+    {/if}
     <h1 class={"text-primary-foreground text-2xl font-bold"}>{title}</h1>
+  </div>  
     <DropdownMenu.Root>
         <DropdownMenu.Trigger asChild let:builder>
           <Button variant="outline" class="pe-5" builders={[builder]}>
